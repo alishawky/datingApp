30a6cd762d4a32f2c94800d98ea061421a5f3ce4
Ali Shawky <alish20030@hotmail.com>
2018-07-25 18:47:23 +0200

    Creating the Message Entity and relationships

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -11,23 +11,39 @@ namespace DatingApp.API.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            //Many to many Likes relationship
             builder.Entity<Like>()
             .HasKey(k => new { k.LikerId, k.LikeeId });
 
-             builder.Entity<Like>()
-             .HasOne(u => u.Likee)
-             .WithMany(u => u.Liker)
-             .HasForeignKey(u => u.LikerId)
-             .OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<Like>()
+            .HasOne(u => u.Likee)
+            .WithMany(u => u.Liker)
+            .HasForeignKey(u => u.LikerId)
+            .OnDelete(DeleteBehavior.Restrict);
 
-              builder.Entity<Like>()
-             .HasOne(u => u.Liker)
-             .WithMany(u => u.Likee)
-             .HasForeignKey(u => u.LikeeId)
-             .OnDelete(DeleteBehavior.Restrict); 
+            builder.Entity<Like>()
+           .HasOne(u => u.Liker)
+           .WithMany(u => u.Likee)
+           .HasForeignKey(u => u.LikeeId)
+           .OnDelete(DeleteBehavior.Restrict);
+
+            //Many to many Messages relationship
+            builder.Entity<Message>()
+            .HasKey(k => new { k.SenderId, k.RecipientId });
+
+            builder.Entity<Message>()
+            .HasOne(u => u.Sender)
+            .WithMany(u => u.MessagesSent)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+             .HasOne(u => u.Recipient)
+             .WithMany(u => u.MessagesReceived)
+             .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
---
+++ DatingApp.API/Migrations/20180725163203_MessageEntityAdded.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class MessageEntityAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    SenderId = table.Column<int>(nullable: false),
+                    RecipientId = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    DateRead = table.Column<DateTime>(nullable: true),
+                    Id = table.Column<int>(nullable: false),
+                    IsRead = table.Column<bool>(nullable: false),
+                    MessageSent = table.Column<DateTime>(nullable: false),
+                    RecipientDeleted = table.Column<bool>(nullable: false),
+                    SenderDeleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => new { x.SenderId, x.RecipientId });
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_RecipientId",
+                        column: x => x.RecipientId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_RecipientId",
+                table: "Messages",
+                column: "RecipientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
